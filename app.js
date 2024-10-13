5c375840d44fccccbc5d27df980577a510a37e5c
Mukhi001 <mukeshgongada1024@gmail.com>
2024-10-14 00:46:51 +0530

    MongoDB basics

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express=require('express');
-
+const mongoose=require('mongoose');
+const Blog=require('./models/blog')
+//connect to mongodb
+const dbURI='mongodb+srv://mukhi:mukhi123@nodetuts.voiwy.mongodb.net/nodetuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+  .then(()=>app.listen(3000))
+  .catch((err)=>console.log(err))
 //express app
 const app=express();
 
@@ -7,15 +13,44 @@ const app=express();
 app.set('view engine','ejs');
 
 
-//listen for requests
-app.listen(3000);
+//mongoose and mongo sandbox routes
+app.get('/add-blog',(req,res)=>{
+const blog=new Blog({
+    title:'new Blog',
+snippet:"about my new blog222222",
+body:'more about my new blog'
+});
+blog.save()
+.then((result)=>{
+res.send(result);
+})
+.catch((err)=>{
+    console.log(err)
+})
+})
 
+app.get('/all-blogs',(req,res)=>{
+    Blog.find()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err=>{
+        console.log(err)
+    }))
+})
+
+app.get('/single-blog',(req,res)=>{
+    Blog.findById('670c197cbcaf15bf65f29579')
+    .then(result=>res.send(result))
+    .catch(err=>console.log(err))
+})
 app.get('/',(req,res)=>{
     const blogs=[
         {title: 'yoshi',snippet:'Lorem ipsum dolor sit amet consectetur'},
         {title: 'mario',snippet:'Lorem ipsum dolor sit amet consectetur'},
         {title: 'hola',snippet:'Lorem ipsum dolor sit amet consectetur'}
     ]
+    console.log(req.url)
        res.render('index',{
         title: 'Home',
         blogs
